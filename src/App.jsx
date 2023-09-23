d912c66a4e408939223a02adccee59bc18b2ee71
Caio Cabral <ocaiocabral@gmail.com>
2023-09-23 10:05:39 -0400

    feat: adding white space to body bottom

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Gallery from "./components/Gallery";
 const FundoGradiente = styled.div`
   background: linear-gradient(174.61deg, #041833 4.16%, #04244F 48%, #154580 96.76%);
   min-height: 100dvh;
+  padding-bottom: 5vh;
 
   & > .grid{
     display: grid;
